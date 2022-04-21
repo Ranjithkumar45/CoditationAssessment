ac64b1ba0272dbe5ca9f5a383dab1a65ccbfa958
Ranjithkumar45 <88719531+Ranjithkumar45@users.noreply.github.com>
2022-04-21 10:55:34 +0530

    Update Connect4.java

--- CoditationAssessment/src/Connect4.java
+++ CoditationAssessment/src/Connect4.java
@@ -4,10 +4,10 @@ public class Connect4 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        char[][] grid = new char[6][7];
-        for (int row = 0; row < grid.length; row++){
-            for (int col = 0; col < grid[0].length; col++){
-                grid[row][col] = ' ';
+        char[][] array = new char[6][7];
+        for (int row = 0; row < array.length; row++){
+            for (int col = 0; col < array[0].length; col++){
+                array[row][col] = ' ';
             }
         }
         int turn = 1;
@@ -18,18 +18,18 @@ public class Connect4 {
             boolean validPlay;
             int play;
             do {
-                display(grid);
+                display(array);
                 System.out.print("Player " + player + ", what column do you want to put your piece: ");
                 play = in.nextInt();
-                validPlay = validate(play,grid);
+                validPlay = validate(play,array);
             }while (validPlay == false);
-            for (int row = grid.length-1; row >= 0; row--){
-                if(grid[row][play] == ' '){
-                    grid[row][play] = player;
+            for (int row = array.length-1; row >= 0; row--){
+                if(array[row][play] == ' '){
+                    array[row][play] = player;
                     break;
                 }
             }
-            winner = isWinner(player,grid);
+            winner = isWinner(player,array);
             if (player == 'R'){
                 player = 'Y';
             }else{
@@ -37,7 +37,7 @@ public class Connect4 {
             }
             turn++;
         }
-        display(grid);
+        display(array);
         if (winner){
             if (player=='R'){
                 System.out.println("Yellow won");
@@ -48,13 +48,13 @@ public class Connect4 {
             System.out.println("Tie game");
         }
     }
-    public static void display(char[][] grid){
+    public static void display(char[][] array){
         System.out.println(" 0 1 2 3 4 5 6");
         System.out.println("---------------");
-        for (int row = 0; row < grid.length; row++){
+        for (int row = 0; row < array.length; row++){
             System.out.print("|");
-            for (int col = 0; col < grid[0].length; col++){
-                System.out.print(grid[row][col]);
+            for (int col = 0; col < array[0].length; col++){
+                System.out.print(array[row][col]);
                 System.out.print("|");
             }
             System.out.println();
@@ -63,52 +63,52 @@ public class Connect4 {
         System.out.println(" 0 1 2 3 4 5 6");
         System.out.println();
     }
-    public static boolean validate(int column, char[][] grid){
-        if (column < 0 || column > grid[0].length){
+    public static boolean validate(int column, char[][] array){
+        if (column < 0 || column > array[0].length){
             return false;
         }
-        if (grid[0][column] != ' '){
+        if (array[0][column] != ' '){
             return false;
         }
         return true;
     }
-    public static boolean isWinner(char player, char[][] grid){
-        for(int row = 0; row<grid.length; row++){
-            for (int col = 0;col < grid[0].length - 3;col++){
-                if (grid[row][col] == player   &&
-                        grid[row][col+1] == player &&
-                        grid[row][col+2] == player &&
-                        grid[row][col+3] == player){
+    public static boolean isWinner(char player, char[][] array){
+        for(int row = 0; row<array.length; row++){
+            for (int col = 0;col < array[0].length - 3;col++){
+                if (array[row][col] == player   &&
+                        array[row][col+1] == player &&
+                        array[row][col+2] == player &&
+                        array[row][col+3] == player){
                     return true;
                 }
             }
         }
-        for(int row = 0; row < grid.length - 3; row++){
-            for(int col = 0; col < grid[0].length; col++){
-                if (grid[row][col] == player   &&
-                        grid[row+1][col] == player &&
-                        grid[row+2][col] == player &&
-                        grid[row+3][col] == player){
+        for(int row = 0; row < array.length - 3; row++){
+            for(int col = 0; col < array[0].length; col++){
+                if (array[row][col] == player   &&
+                        array[row+1][col] == player &&
+                        array[row+2][col] == player &&
+                        array[row+3][col] == player){
                     return true;
                 }
             }
         }
-        for(int row = 3; row < grid.length; row++){
-            for(int col = 0; col < grid[0].length - 3; col++){
-                if (grid[row][col] == player   &&
-                        grid[row-1][col+1] == player &&
-                        grid[row-2][col+2] == player &&
-                        grid[row-3][col+3] == player){
+        for(int row = 3; row < array.length; row++){
+            for(int col = 0; col < array[0].length - 3; col++){
+                if (array[row][col] == player   &&
+                        array[row-1][col+1] == player &&
+                        array[row-2][col+2] == player &&
+                        array[row-3][col+3] == player){
                     return true;
                 }
             }
         }
-        for(int row = 0; row < grid.length - 3; row++){
-            for(int col = 0; col < grid[0].length - 3; col++){
-                if (grid[row][col] == player   &&
-                        grid[row+1][col+1] == player &&
-                        grid[row+2][col+2] == player &&
-                        grid[row+3][col+3] == player){
+        for(int row = 0; row < array.length - 3; row++){
+            for(int col = 0; col < array[0].length - 3; col++){
+                if (array[row][col] == player   &&
+                        array[row+1][col+1] == player &&
+                        array[row+2][col+2] == player &&
+                        array[row+3][col+3] == player){
                     return true;
                 }
             }
